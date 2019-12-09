f376c695c915a8289aa8f5fffed4f8ab0a973b75
Darrmitha <darrmitha.kumar@gmail.com>
2019-12-09 14:47:17 +0800

    init

--- app/src/main/java/com/example/insurance_premier_calculator/MainActivity.kt
+++ app/src/main/java/com/example/insurance_premier_calculator/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             myData.premiumAmount = getPremium()
             display()
 
+            
         }
 
         btnReset.setOnClickListener {
